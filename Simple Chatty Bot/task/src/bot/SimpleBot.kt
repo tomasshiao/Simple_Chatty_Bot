b450adfa48ec2a9ac6e2591e7f860ef15d0618f5
Tomás Shiao <31970583+tomasshiao@users.noreply.github.com>
2020-08-04 18:14:45 -0300

    Initial commit

---
+++ Simple Chatty Bot/task/src/bot/SimpleBot.kt
@@ -0,0 +1,23 @@
+package bot
+
+import java.util.*
+
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+
+    println("Hello! My name is Aid.")
+    println("I was created in 2020.")
+    println("Please, remind me your name.")
+
+    // reading a name
+
+    println("What a great name you have, {yourName}!")
+    println("Let me guess your age.")
+    println("Enter remainders of dividing your age by 3, 5 and 7.")
+
+    // reading all remainders
+
+    println("Your age is {yourAge}; that's a good time to start programming!")
+}
+
