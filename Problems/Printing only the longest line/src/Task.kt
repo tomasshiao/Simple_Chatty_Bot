674389f33d02b02215c37925eb3e37386a194895
Tomás Shiao <31970583+tomasshiao@users.noreply.github.com>
2020-08-08 00:05:19 -0300

    Added Comment module's tasks.

--- Problems/Printing only the longest line/src/Task.kt
+++ Problems/Printing only the longest line/src/Task.kt
@@ -1,8 +1,8 @@
 fun main() {
-    //println("Print it!")
-    // println("No, print it!")
-    /* println("Uncomment this line!") */
+    /*println("Print it!")
+    println("No, print it!")
+    println("Uncomment this line!") */
     println("Please, comment this line!")
-    //println("Display this text!")
-    //// println("Do not comment it!")
+    /*println("Display this text!")
+    println("Do not comment it!")*/
 }
